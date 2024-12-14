ebd1f05be4661021e544db2a2538288f3d62ea18
Difa Winanda Adliya <difwinda@gmail.com>
2024-12-14 08:40:37 +0700

    update balance and transaction history on home tab

--- app/(home)/_layout.jsx
+++ app/(home)/_layout.jsx
@@ -1,4 +1,5 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
@@ -12,19 +13,19 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="transfer"
-        options={{
-          title: 'Transfer',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="send" color={color} />,
-        }}
-      />
-      <Tabs.Screen
         name="topup"
         options={{
           title: 'Top Up',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="upload" color={color} />,
+          tabBarIcon: ({ color }) => <FontAwesome6 size={28} name="add" color={color} />,
         }}
       />
+        <Tabs.Screen
+          name="transfer"
+          options={{
+            title: 'Transfer',
+            tabBarIcon: ({ color }) => <FontAwesome size={28} name="send" color={color} />,
+          }}
+        />
     </Tabs>
   );
 }
